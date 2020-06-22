a30fc364263738b427f545f686bb3233c9383cca
Viviane <vivianedias@protonmail.com>
2020-06-22 01:00:27 -0300

    feat(main): tie all the logics together

--- packages/listener-match/src/components/index.ts
+++ packages/listener-match/src/components/index.ts
@@ -1,5 +1,5 @@
 import dbg from "../dbg";
-import { SubscriptionResponse } from "../types";
+import { SubscriptionResponse, Volunteer } from "../types";
 import {
   getRequestedVolunteerType,
   getVolunteerOrganizationId
@@ -14,8 +14,26 @@ import {
   forwardPublicService,
   updateIndividualTicket
 } from "./Individual";
+import {
+  createMatchTicket,
+  updateSolidarityTickets
+} from "../graphql/mutations";
 
 const log = dbg.extend("match");
+
+const syncTickets = async (ids: number[]) => {
+  const ticket = {
+    match_syncronized: true
+  };
+  const sync = await updateSolidarityTickets(ticket, ids);
+  if (!sync) {
+    log(`Couldn't update sync status from MSR tickets: ${ids}`);
+    return undefined;
+  }
+  log(`Tickets that passed through match: ${ids}`);
+  log("Match is done");
+  return true;
+};
 
 export const handleMatch = () => async (response: SubscriptionResponse) => {
   log(`${new Date()}: \nReceiving data on subscription GraphQL API...`);
@@ -24,53 +42,74 @@ export const handleMatch = () => async (response: SubscriptionResponse) => {
     data: { solidarity_tickets: tickets }
   } = response;
   const volunteers_available = await fetchVolunteersAvailable();
-  const matchs = tickets
-    .filter(ticket => {
-      if (ticket.status_acolhimento === "solicitaçao_repetida") {
-        return ticket.atrelado_ao_ticket !== null;
-      }
-      return true;
-    })
-    .map(async ticket => {
-      const {
-        subject,
-        status_acolhimento,
-        atrelado_ao_ticket,
-        requester_id,
-        ticket_id
-      } = ticket;
-      const volunteer_type = getRequestedVolunteerType(subject);
-      if (!volunteer_type) return ticket.ticket_id;
 
-      const volunteer_organization_id = getVolunteerOrganizationId(
-        volunteer_type
-      );
-      const individual = await fetchIndividual(requester_id);
-      if (individual.length < 1) return ticket_id;
-      const filteredVolunteers = volunteers_available.filter(
-        ({ organization_id }) => organization_id === volunteer_organization_id
-      );
-      // log(filteredVolunteers);
-      // log(individual);
-      const fetchVolunteer = getClosestVolunteer(
-        individual[0],
-        filteredVolunteers
-      );
+  const matchs = tickets.flatMap(async individualTicket => {
+    const {
+      subject,
+      status_acolhimento,
+      atrelado_ao_ticket,
+      requester_id,
+      ticket_id
+    } = individualTicket;
 
-      if (!fetchVolunteer)
-        return forwardPublicService(ticket_id, individual[0].state);
+    if (
+      status_acolhimento === "solicitaçao_repetida" &&
+      atrelado_ao_ticket === null
+    )
+      return undefined;
 
-      // log({ ticket, fetchVolunteer });
+    const volunteer_type = getRequestedVolunteerType(subject);
+    if (!volunteer_type) return ticket_id;
+    const volunteer_organization_id = getVolunteerOrganizationId(
+      volunteer_type
+    );
+    const filteredVolunteers = volunteers_available.filter(
+      ({ organization_id }) => organization_id === volunteer_organization_id
+    );
 
-      createVolunteerTicket(fetchVolunteer);
-      if (status_acolhimento === "solicitação_repetida" && atrelado_ao_ticket) {
-        updateIndividualTicket({ ...ticket, ticket_id: atrelado_ao_ticket });
-        return [ticket_id, atrelado_ao_ticket];
-      }
-      updateIndividualTicket(ticket);
-      return ticket_id;
-    });
-  return matchs;
+    const individual = await fetchIndividual(requester_id);
+    if (individual.length < 1) return ticket_id;
+
+    const volunteer = getClosestVolunteer(individual[0], filteredVolunteers);
+    if (!volunteer)
+      return await forwardPublicService(ticket_id, individual[0].state);
+
+    const volunteer_ticket_id = await createVolunteerTicket(
+      volunteer,
+      individualTicket
+    );
+    if (!volunteer_ticket_id) return undefined;
+    volunteer["ticket_id"] = volunteer_ticket_id;
+
+    const newTicketId = ticket_id;
+    if (status_acolhimento === "solicitação_repetida" && atrelado_ao_ticket) {
+      individualTicket["ticket_id"] = atrelado_ao_ticket;
+    }
+
+    const updateIndividual = await updateIndividualTicket(
+      individualTicket,
+      volunteer as Volunteer & { ticket_id: number }
+    );
+    if (!updateIndividual) return undefined;
+
+    const matchTicket = await createMatchTicket(
+      individualTicket,
+      volunteer as Volunteer & { ticket_id: number }
+    );
+    if (!matchTicket) return undefined;
+    return individualTicket["ticket_id"] !== newTicketId
+      ? [newTicketId, updateIndividual]
+      : updateIndividual;
+  });
+
+  return Promise.all(matchs).then(async m => {
+    const sync_tickets = m.filter(match => !!match);
+    if (sync_tickets.length < 1) {
+      log("No tickets to sync");
+      return undefined;
+    }
+    return await syncTickets(sync_tickets as number[]);
+  });
 };
 
 export default handleMatch;
