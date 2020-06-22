7b0161bbdb2135a14d116e158ac8e64f9afbc9f2
Viviane <vivianedias@protonmail.com>
2020-06-22 00:47:08 -0300

    feat(public-service): deals with individuals that didnt have volunteers around

--- packages/listener-match/src/components/Individual/forwardPublicService.ts
+++ packages/listener-match/src/components/Individual/forwardPublicService.ts
@@ -1,8 +1,89 @@
+import * as yup from "yup";
+import { updateTicket } from "../Zendesk";
+import { getCurrentDate, composeCustomFields } from "../../services/utils";
 import dbg from "../../dbg";
+import { updateSolidarityTickets } from "../../graphql/mutations";
 
-const log = dbg.extend("forwardPublicService");
+const log = dbg.extend("updateIndividualTicket");
 
-export default async (id: number, state: string) => {
-  log(id, state);
-  return [id, state];
+const AGENT = Number(process.env.AGENT_ID) || 0;
+
+const hasuraSchema = yup
+  .object()
+  .shape({
+    status: yup.string().required(),
+    assignee_id: yup.number().required(),
+    custom_fields: yup
+      .array()
+      .of(
+        yup
+          .object()
+          .shape({
+            id: yup.number(),
+            value: yup.string().nullable()
+          })
+          .required()
+      )
+      .min(3)
+      .required(),
+    description: yup.string().required(),
+    updated_at: yup.string().required(),
+    data_encaminhamento: yup.string().required(),
+    status_acolhimento: yup.string().required(),
+    ticket_id: yup.number().required()
+  })
+  .required();
+
+export default async (ticket_id: number, state: string) => {
+  log("Entering forwardPublicService");
+  const ticket = {
+    status: "pending",
+    assignee_id: AGENT,
+    custom_fields: [
+      {
+        id: 360021879791,
+        value: state
+      },
+      {
+        id: 360014379412,
+        value: "encaminhamento__realizado_para_serviço_público"
+      },
+      {
+        id: 360017432652,
+        value: String(getCurrentDate())
+      }
+    ],
+    comment: {
+      body: `Ticket da MSR foi atualizado após ela ser encaminhada para um serviço público`,
+      author_id: AGENT,
+      public: false
+    }
+  };
+  try {
+    const zendeskTicket = await updateTicket(ticket_id, ticket);
+    if (!zendeskTicket) {
+      throw new Error("Zendesk ticket update returned errors");
+    }
+
+    log("Preparing ticket to be saved in Hasura");
+    const hasuraTicket = {
+      ...zendeskTicket,
+      ...composeCustomFields(zendeskTicket.custom_fields),
+      ticket_id: zendeskTicket.id
+    };
+    // log({ hasuraTicket: JSON.stringify(hasuraTicket, null, 2) });
+
+    const validatedTicket = await hasuraSchema.validate(hasuraTicket, {
+      stripUnknown: true
+    });
+
+    const inserted = await updateSolidarityTickets(validatedTicket, [
+      validatedTicket.ticket_id
+    ]);
+    if (!inserted) return undefined;
+    return inserted && inserted[0] && inserted[0].ticket_id;
+  } catch (e) {
+    log("failed to create ticket in zendesk: ".red, e);
+    return undefined;
+  }
 };
