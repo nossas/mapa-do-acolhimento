aad2cf0b4b71ca55dd58a1cdee2d6cebc972651e
Viviane Dias <vivipirolad@gmail.com>
2020-09-15 22:47:22 -0300

    feat(mautic-zendesk): removes repeated code  and removes falsy values when saving user

--- packages/webhooks-mautic-zendesk/src/integrations/AdvogadaCreateUser.ts
+++ packages/webhooks-mautic-zendesk/src/integrations/AdvogadaCreateUser.ts
@@ -1,14 +1,13 @@
-import { getGeolocation } from "bonde-core-tools";
 import * as yup from "yup";
 import { Response } from "express";
 import Base from "./Base";
-
-export enum CONDITION {
-  UNSET = "unset",
-  REPROVADA_REGISTRO_INVÁLIDO = "reprovada_registro_inválido",
-  REPROVADA_DIRETRIZES_DO_MAPA = "reprovada_diretrizes_do_mapa",
-  REPROVADA_ESTUDO_DE_CASO = "reprovada_estudo_de_caso"
-}
+import { CONDITION } from "../types";
+import {
+  verificaEstudoDeCaso,
+  verificaLocalização,
+  verificaDiretrizesAtendimento,
+  removeFalsyValues
+} from "../utils";
 
 interface InputFromMautic {
   createdAt: string;
@@ -23,157 +22,15 @@ class AdvogadaCreateUser extends Base {
     super("AdvogadaCreateUser", "users/create_or_update", res);
   }
 
-  private setCondition = (condition: [CONDITION], value: CONDITION) => {
-    const newCondition = condition;
-    if (newCondition[0] === CONDITION.UNSET) {
-      newCondition[0] = value;
-    }
-  };
-
-  private verificaDiretrizesAtendimento = async (
-    condition: [CONDITION],
-    data
-  ) => {
-    let newData = data;
-    const verificaCamposDiretrizesAtendimento = yup
-      .object()
-      .shape({
-        todos_os_atendimentos_rea: yup.string().required(),
-        as_voluntarias_do_mapa_do: yup.string().required(),
-        o_comprometimento_a_dedic: yup.string().required(),
-        o_mapa_do_acolhimento_ent: yup.string().required(),
-        para_que_as_mulheres_que: yup.string().required()
-      })
-      .required();
-
-    try {
-      await verificaCamposDiretrizesAtendimento.validate(newData, {
-        strict: true
-      });
-    } catch (e) {
-      this.setCondition(condition, CONDITION.REPROVADA_REGISTRO_INVÁLIDO);
-    }
-
-    const verificaRespostasDiretrizesAtendimento = yup
-      .object()
-      .shape({
-        todos_os_atendimentos_rea: yup.string().matches(/aceito/),
-        as_voluntarias_do_mapa_do: yup.string().matches(/compreendo/),
-        o_comprometimento_a_dedic: yup.string().matches(/sim/),
-        o_mapa_do_acolhimento_ent: yup.string().matches(/sim/),
-        para_que_as_mulheres_que: yup.string().matches(/sim/)
-      })
-      .required();
-
-    if (!(await verificaRespostasDiretrizesAtendimento.isValid(newData))) {
-      this.setCondition(condition, CONDITION.REPROVADA_DIRETRIZES_DO_MAPA);
-    }
-
-    const stripDiretrizesAtendimento = yup.object().shape({
-      todos_os_atendimentos_rea: yup.mixed().strip(true),
-      as_voluntarias_do_mapa_do: yup.mixed().strip(true),
-      o_comprometimento_a_dedic: yup.mixed().strip(true),
-      o_mapa_do_acolhimento_ent: yup.mixed().strip(true),
-      para_que_as_mulheres_que: yup.mixed().strip(true)
-    });
-
-    newData = await stripDiretrizesAtendimento.cast(newData);
-
-    return newData;
-  };
-
-  private verificaEstudoDeCaso = async (condition: [CONDITION], data) => {
-    let newData = data;
-    const verificaCamposEstudoDeCaso = yup
-      .object()
-      .shape({
-        no_seu_primeiro_atendimen: yup.string().required(),
-        para_voce_o_que_e_mais_im: yup.string().required(),
-        durante_os_encontros_ana: yup.string().required(),
-        durante_os_atendimentos_a: yup.string().required()
-      })
-      .required();
-
-    try {
-      await verificaCamposEstudoDeCaso.validate(newData, {
-        strict: true
-      });
-    } catch (e) {
-      this.setCondition(condition, CONDITION.REPROVADA_REGISTRO_INVÁLIDO);
-    }
-
-    const verificaRespostaEstudoDeCaso = yup
-      .object()
-      .shape({
-        no_seu_primeiro_atendimen: yup.string().matches(/A|B/),
-        para_voce_o_que_e_mais_im: yup.string().matches(/A|B/),
-        durante_os_encontros_ana: yup.string().matches(/A|B/),
-        durante_os_atendimentos_a: yup.string().matches(/A|B/)
-      })
-      .required();
-
-    if (!(await verificaRespostaEstudoDeCaso.isValid(newData))) {
-      this.setCondition(condition, CONDITION.REPROVADA_ESTUDO_DE_CASO);
-    }
-
-    const stripRespostaEstudoDeCaso = yup
-      .object()
-      .shape({
-        no_seu_primeiro_atendimen: yup.mixed().strip(true),
-        para_voce_o_que_e_mais_im: yup.mixed().strip(true),
-        durante_os_encontros_ana: yup.mixed().strip(true),
-        durante_os_atendimentos_a: yup.mixed().strip(true)
-      })
-      .required();
-
-    newData = await stripRespostaEstudoDeCaso.cast(newData);
-
-    return newData;
-  };
-
-  /**
-   * TODO: adicionar tag cep_incorreto
-   */
-  private verificaLocalização = async (_condition: [CONDITION], data) => {
-    let newData = data;
-    const verificaCep = yup
-      .object()
-      .shape({
-        cep: yup.string().nullable()
-      })
-      .required();
-    try {
-      newData = await verificaCep.validate(newData);
-    } catch (e) {
-      return {};
-      // this.setCondition(condition, CONDITION.REPROVADA_REGISTRO_INVÁLIDO)
-    }
-    const { latitude, longitude, ...rest } = await getGeolocation({
-      cep: newData.cep,
-      email: newData.email
-    });
-
-    const tags: string[] | undefined =
-      latitude === null || longitude === null ? ["cep-incorreto"] : undefined;
-
-    return {
-      ...newData,
-      ...rest,
-      latitude,
-      longitude,
-      tags
-    };
-  };
-
   start = async (data, { createdAt, name, cep }: InputFromMautic) => {
     let newData = {
       ...data,
       cep
     };
     const condition: [CONDITION] = [CONDITION.UNSET];
-    newData = await this.verificaDiretrizesAtendimento(condition, newData);
-    newData = await this.verificaEstudoDeCaso(condition, newData);
-    const validatedResult = await this.verificaLocalização(condition, newData);
+    newData = await verificaDiretrizesAtendimento(condition, newData);
+    newData = await verificaEstudoDeCaso(condition, newData);
+    const validatedResult = await verificaLocalização(condition, newData);
 
     const { tags } = validatedResult;
 
@@ -202,7 +59,7 @@ class AdvogadaCreateUser extends Base {
             phone,
             organization_id: this.organizations[this.organization],
             user_fields: {
-              ...userFields,
+              ...removeFalsyValues(userFields),
               data_de_inscricao_no_bonde: createdAt,
               ultima_atualizacao_de_dados: new Date().toString(),
               condition: condition[0] === "unset" ? "aprovada" : condition[0],
--- packages/webhooks-mautic-zendesk/src/integrations/PsicologaCreateUser.ts
+++ packages/webhooks-mautic-zendesk/src/integrations/PsicologaCreateUser.ts
@@ -1,14 +1,13 @@
-import { getGeolocation } from "bonde-core-tools";
 import * as yup from "yup";
 import { Response } from "express";
 import Base from "./Base";
-
-export enum CONDITION {
-  UNSET = "unset",
-  REPROVADA_REGISTRO_INVÁLIDO = "reprovada_registro_inválido",
-  REPROVADA_DIRETRIZES_DO_MAPA = "reprovada_diretrizes_do_mapa",
-  REPROVADA_ESTUDO_DE_CASO = "reprovada_estudo_de_caso"
-}
+import { CONDITION } from "../types";
+import {
+  verificaEstudoDeCaso,
+  verificaLocalização,
+  verificaDiretrizesAtendimento,
+  removeFalsyValues
+} from "../utils";
 
 interface InputFromMautic {
   createdAt: string;
@@ -23,164 +22,15 @@ class PsicologaCreateUser extends Base {
     super("PsicólogaCreateUser", "users/create_or_update", res);
   }
 
-  private setCondition = (condition: [CONDITION], value: CONDITION) => {
-    const newCondition = condition;
-    if (condition[0] === CONDITION.UNSET) {
-      newCondition[0] = value;
-    }
-  };
-
-  private verificaDiretrizesAtendimento = async (
-    condition: [CONDITION],
-    data: Record<string, unknown>
-  ) => {
-    let newData = data;
-    const verificaCamposDiretrizesAtendimento = yup
-      .object()
-      .shape({
-        todos_os_atendimentos_rea: yup.string().required(),
-        as_voluntarias_do_mapa_do: yup.string().required(),
-        o_comprometimento_a_dedic: yup.string().required(),
-        o_mapa_do_acolhimento_ent: yup.string().required(),
-        para_que_as_mulheres_que: yup.string().required()
-      })
-      .required();
-
-    try {
-      await verificaCamposDiretrizesAtendimento.validate(newData, {
-        strict: true
-      });
-    } catch (e) {
-      this.setCondition(condition, CONDITION.REPROVADA_REGISTRO_INVÁLIDO);
-    }
-
-    const verificaRespostasDiretrizesAtendimento = yup
-      .object()
-      .shape({
-        todos_os_atendimentos_rea: yup.string().matches(/aceito/),
-        as_voluntarias_do_mapa_do: yup.string().matches(/compreendo/),
-        o_comprometimento_a_dedic: yup.string().matches(/sim/),
-        o_mapa_do_acolhimento_ent: yup.string().matches(/sim/),
-        para_que_as_mulheres_que: yup.string().matches(/sim/)
-      })
-      .required();
-
-    if (!(await verificaRespostasDiretrizesAtendimento.isValid(newData))) {
-      this.setCondition(condition, CONDITION.REPROVADA_DIRETRIZES_DO_MAPA);
-    }
-
-    const stripDiretrizesAtendimento = yup
-      .object()
-      .shape({
-        todos_os_atendimentos_rea: yup.mixed().strip(true),
-        as_voluntarias_do_mapa_do: yup.mixed().strip(true),
-        o_comprometimento_a_dedic: yup.mixed().strip(true),
-        o_mapa_do_acolhimento_ent: yup.mixed().strip(true),
-        para_que_as_mulheres_que: yup.mixed().strip(true)
-      })
-      .required();
-
-    newData = stripDiretrizesAtendimento.cast(newData);
-
-    return newData;
-  };
-
-  private verificaEstudoDeCaso = async (
-    condition: [CONDITION],
-    data: object
-  ) => {
-    let newData = data;
-    const verificaCamposEstudoDeCaso = yup
-      .object()
-      .shape({
-        no_seu_primeiro_atendimen: yup.string().required(),
-        para_voce_o_que_e_mais_im: yup.string().required(),
-        durante_os_encontros_ana: yup.string().required(),
-        durante_os_atendimentos_a: yup.string().required()
-      })
-      .required();
-
-    try {
-      await verificaCamposEstudoDeCaso.validate(newData, {
-        strict: true
-      });
-    } catch (e) {
-      this.setCondition(condition, CONDITION.REPROVADA_REGISTRO_INVÁLIDO);
-    }
-
-    const verificaRespostaEstudoDeCaso = yup
-      .object()
-      .shape({
-        no_seu_primeiro_atendimen: yup.string().matches(/A|B/),
-        para_voce_o_que_e_mais_im: yup.string().matches(/A|B/),
-        durante_os_encontros_ana: yup.string().matches(/A|B/),
-        durante_os_atendimentos_a: yup.string().matches(/A|B/)
-      })
-      .required();
-
-    if (!(await verificaRespostaEstudoDeCaso.isValid(newData))) {
-      this.setCondition(condition, CONDITION.REPROVADA_ESTUDO_DE_CASO);
-    }
-
-    const stripRespostaEstudoDeCaso = yup
-      .object()
-      .shape({
-        no_seu_primeiro_atendimen: yup.mixed().strip(true),
-        para_voce_o_que_e_mais_im: yup.mixed().strip(true),
-        durante_os_encontros_ana: yup.mixed().strip(true),
-        durante_os_atendimentos_a: yup.mixed().strip(true)
-      })
-      .required();
-
-    newData = await stripRespostaEstudoDeCaso.cast(newData);
-
-    return newData;
-  };
-
-  private verificaLocalização = async (
-    _condition: [CONDITION],
-    data: object
-  ) => {
-    const verificaCep = yup
-      .object()
-      .shape({
-        cep: yup.string().nullable()
-      })
-      .required();
-    let verifiedData;
-    try {
-      verifiedData = await verificaCep.validate(data);
-    } catch (e) {
-      return {};
-      // this.setCondition(condition, CONDITION.REPROVADA_REGISTRO_INVÁLIDO)
-    }
-
-    const { latitude, longitude, ...rest } = await getGeolocation({
-      cep: verifiedData.cep,
-      email: verifiedData.email
-    });
-
-    const tags: string[] | undefined =
-      latitude === null || longitude === null ? ["cep-incorreto"] : undefined;
-
-    return {
-      ...verifiedData,
-      ...rest,
-      latitude,
-      longitude,
-      tags
-    };
-  };
-
   start = async (data, { createdAt, name, cep }: InputFromMautic) => {
     let newData = {
       ...data,
       cep
     };
     const condition: [CONDITION] = [CONDITION.UNSET];
-    newData = await this.verificaDiretrizesAtendimento(condition, newData);
-    newData = await this.verificaEstudoDeCaso(condition, newData);
-    const validatedResult = await this.verificaLocalização(condition, newData);
+    newData = await verificaDiretrizesAtendimento(condition, newData);
+    newData = await verificaEstudoDeCaso(condition, newData);
+    const validatedResult = await verificaLocalização(condition, newData);
 
     const { tags } = validatedResult;
 
@@ -209,7 +59,7 @@ class PsicologaCreateUser extends Base {
             phone,
             organization_id: this.organizations[this.organization],
             user_fields: {
-              ...userFields,
+              ...removeFalsyValues(userFields),
               data_de_inscricao_no_bonde: createdAt,
               ultima_atualizacao_de_dados: new Date().toString(),
               condition: condition[0] === "unset" ? "aprovada" : condition[0],
