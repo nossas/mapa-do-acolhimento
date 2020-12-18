268a72d6f6f423b0fb5e07c4a579cd7d8e100e31
Viviane Dias <vivianedias@protonmail.com>
2020-12-18 15:09:33 -0300

    feat(client): remove client

--- packages/client-solidarity/src/components/Popups/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Popup } from "./Popup";
-export { default as Confirm } from "./Confirm";
-export { default as Success } from "./Success";
-export { default as Warning } from "./Warning";
-export { default as Error } from "./Error";
