083ce1d399bb4bb0a2af72b0e0d0aa736999063e
Viviane Dias <vivianedias@protonmail.com>
2021-05-08 19:17:08 -0300

    feat(listener-solidarity): initiates apm in service

--- packages/listener-solidarity/src/main.ts
+++ packages/listener-solidarity/src/main.ts
@@ -1,9 +1,16 @@
 import throng from "throng";
+import apm from "elastic-apm-node";
 import { subscriptionFormEntries } from "./graphql/subscriptions";
 import widgets from "./form_entries_mapping";
 import logger from "./logger";
 
 const log = logger.child({ module: "main" });
+
+const {
+  ELASTIC_APM_SECRET_TOKEN: secretToken,
+  ELASTIC_APM_SERVER_URL: serverUrl,
+  ELASTIC_APM_SERVICE_NAME: serviceName
+} = process.env;
 
 throng({
   workers: 1,
@@ -11,6 +18,14 @@ throng({
     log.info(`Started worker ${id}`);
 
     try {
+      if (secretToken && serverUrl && serviceName) {
+        apm.start({
+          secretToken,
+          serverUrl,
+          serviceName
+        });
+      }
+
       log.info("Fetching solidarity users...");
       log.info(
         "Call subscriptions to form_entries... %s",
