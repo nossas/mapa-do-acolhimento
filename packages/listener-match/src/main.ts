037558575c2cf98e7deea0f3b29add2404f8ae73
Viviane Dias <vivianedias@protonmail.com>
2021-05-08 19:04:37 -0300

    feat(listener-match): initiates apm on service start

--- packages/listener-match/src/main.ts
+++ packages/listener-match/src/main.ts
@@ -1,8 +1,15 @@
 import throng from "throng";
+import apm from "elastic-apm-node";
 import { subscriptionSolidarityTickets } from "./graphql/subscriptions";
 import dbg from "./dbg";
 
 const log = dbg.child({ module: "main" });
+
+const {
+  ELASTIC_APM_SECRET_TOKEN: secretToken,
+  ELASTIC_APM_SERVER_URL: serverUrl,
+  ELASTIC_APM_SERVICE_NAME: serviceName
+} = process.env;
 
 throng({
   workers: 1,
@@ -10,6 +17,13 @@ throng({
     log.info(`Started worker ${id}`);
 
     try {
+      if (secretToken && serverUrl && serviceName) {
+        apm.start({
+          secretToken,
+          serverUrl,
+          serviceName
+        });
+      }
       log.info("Fetching solidarity tickets for match...");
       await subscriptionSolidarityTickets();
     } catch (err) {
