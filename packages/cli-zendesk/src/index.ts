88068a5fede84dba1326771d354751a4a627169d
Viviane Dias <vivianedias@protonmail.com>
2021-05-16 20:01:59 -0300

    feat(cli-zendesk): initiates apm before worker and passes apm agent to App

--- packages/cli-zendesk/src/index.ts
+++ packages/cli-zendesk/src/index.ts
@@ -1,11 +1,5 @@
 import dotenv from "dotenv";
 import apm from "elastic-apm-node";
-import { install } from "source-map-support";
-import commander from "commander";
-import signale from "signale";
-import checkConfig from "./checkConfig";
-import dbg from "./dbg";
-import App from "./App";
 
 dotenv.config();
 
@@ -15,21 +9,25 @@ const {
   ELASTIC_APM_SERVICE_NAME: serviceName
 } = process.env;
 
-install();
+let apmAgent;
 
 if (secretToken && serverUrl && serviceName) {
-  console.log(
-    "Estou tentando conectar com o APM",
+  apmAgent = apm.start({
     secretToken,
     serverUrl,
-    serviceName
-  );
-  apm.start({
-    secretToken,
-    serverUrl,
-    serviceName
+    serviceName,
+    environment: process.env.NODE_ENV
   });
 }
+
+import { install } from "source-map-support";
+import commander from "commander";
+import signale from "signale";
+import checkConfig from "./checkConfig";
+import dbg from "./dbg";
+import App from "./App";
+
+install();
 
 const program = new commander.Command();
 program.option(
@@ -44,11 +42,11 @@ const init = async () => {
     program.parse(process.argv);
     if (program.mode === "ticket") {
       signale.time("duration");
-      await App.ticket();
+      await App.ticket(apmAgent);
       signale.timeEnd("duration");
     } else if (program.mode === "user") {
       signale.time("duration");
-      await App.user();
+      await App.user(apmAgent);
       signale.timeEnd("duration");
     } else if (program.mode === undefined) {
       signale.fatal("operation mode is required");
@@ -56,6 +54,7 @@ const init = async () => {
       signale.fatal(`'${program.mode}' is not a valid operation mode`);
     }
   } catch (e) {
+    apmAgent.captureError(e);
     dbg.error(e);
   }
 };
