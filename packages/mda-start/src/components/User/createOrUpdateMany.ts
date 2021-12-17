c1dc121534b8e8229f6baad5857a23089303f407
Adriele A Secundino <dri.leela@gmail.com>
2021-12-17 10:44:30 -0300

    chore(mda-start): add function createOrUpdateMany

---
+++ packages/mda-start/src/components/User/createOrUpdateMany.ts
@@ -0,0 +1,44 @@
+import { User, ZendeskUserCreationResponse } from "../../types";
+import fetch from "cross-fetch";
+import jobStatuses from "./jobStatuses";
+import logger from "../../logger";
+
+const log = logger.child({ labels: { process: "createOrUpdateMany" } });
+
+const parameters = {
+  username: process.env.ZENDESK_API_USER || "",
+  token: process.env.ZENDESK_API_TOKEN || "",
+  remoteUri: process.env.ZENDESK_API_URL || ""
+}
+
+const getAuth = () => ({          
+  headers: {
+    Authorization: 'Basic ' + Buffer.from(parameters.username + "/token:" + parameters.token).toString('base64'),
+    'content-type': 'application/json'
+  }                     
+})
+
+const createOrUpdateMany = (users: User[]): Promise <ZendeskUserCreationResponse> => {
+  
+  return fetch(`${parameters.remoteUri}users/create_or_update_many.json`, {
+    method: 'POST',
+    body: JSON.stringify({ users }),
+    ...getAuth()
+  })
+  .then((response) => { 
+    if(response.status === 200){
+      return response.json()
+    }
+    throw new Error(response.statusText);
+  })
+  .then((result) => {
+    const jobID = result["job_status"].id;
+    return jobStatuses(jobID, 5000).then((data) => data as ZendeskUserCreationResponse); 
+  })
+  .catch((err)=>{
+    log.error(err);
+    return err;
+  })
+}
+
+export default createOrUpdateMany;
--- packages/mda-start/src/components/User/jobStatuses.ts
+++ packages/mda-start/src/components/User/jobStatuses.ts
@@ -1,51 +1,54 @@
 import fetch from "cross-fetch";
+import logger from "../../logger";
+
+const log = logger.child({ labels: { process: "jobStatuses" } });
 
 const parameters = {
-    username: process.env.ZENDESK_API_USER || "",
-    token: process.env.ZENDESK_API_TOKEN || "",
-    remoteUri: process.env.ZENDESK_API_URL || ""
+  username: process.env.ZENDESK_API_USER || "",
+  token: process.env.ZENDESK_API_TOKEN || "",
+  remoteUri: process.env.ZENDESK_API_URL || ""
 }
-console.log(parameters);
+
 const getAuth = () => ({          
-    headers: {
-        Authorization: 'Basic ' + Buffer.from(parameters.username + ":" + parameters.token).toString('base64')
-    }                     
+  headers: {
+    Authorization: 'Basic ' + Buffer.from(parameters.username + "/token:" + parameters.token).toString('base64')
+  }                     
 })
 
 const jobStatuses = (job_status_id: string, interval?: number) => new Promise((resolve, reject)=>{
-    const nIntervId = setInterval(function getJobStatusUntilComplete() { getJobStatus(); },(interval || 5000)); 
-    
-    //controla ciclo de atualização do status
-    const getJobStatus = () => {
-        fetch(`${parameters.remoteUri}job_statuses/${job_status_id}.json`, getAuth())
-            .then((response) => { 
-                if(response.status === 200){
-                    return response.json()
-                }
-                throw new Error(response.statusText);
-            })
-            .then((result)=> {
-                if(result["job_status"].status === "completed" ||
-                    result["job_status"].status === "failed" || 
-                    result["job_status"].status === "killed") {
-                    
-                    //resultado com sucesso para a thread
-                    clearInterval(nIntervId);
-                    console.log('Job '+job_status_id+' completed!');
-                    return resolve(result)
-                } else {
-                    console.log('Job prgress: ' + result["job_status"].progress + ' out of ' + result["job_status"].total);
-                }
-            })
-            .catch((err)=> {
-                console.log(err);
-
-                //resultado com erro para thread
-                clearInterval(nIntervId);
-                return reject(err);
-            });
+  const nIntervId = setInterval(function getJobStatusUntilComplete() { getJobStatus(); },(interval || 5000)); 
+  
+  //controla ciclo de atualização do status
+  const getJobStatus = () => {
+    fetch(`${parameters.remoteUri}job_statuses/${job_status_id}.json`, getAuth())
+    .then((response) => { 
+      if(response.status === 200){
+        return response.json()
+      }
+      throw new Error(response.statusText);
+    })
+    .then((result)=> {
+      if(result["job_status"].status === "completed" ||
+      result["job_status"].status === "failed" || 
+      result["job_status"].status === "killed") {
         
-    }
+        //resultado com sucesso para a thread
+        clearInterval(nIntervId);
+        log.info('Job '+job_status_id+' completed!');
+        return resolve(result)
+      } else {
+        log.info('Job prgress: ' + result["job_status"].progress + ' out of ' + result["job_status"].total);
+      }
+    })
+    .catch((err)=> {
+      log.error(err);
+      
+      //resultado com erro para thread
+      clearInterval(nIntervId);
+      return reject(err);
+    });
+    
+  }
 });
 
 export default jobStatuses;
