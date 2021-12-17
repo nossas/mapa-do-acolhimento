07ecbd75399cd9834bfe052ca4c3995c06e99340
Adriele A Secundino <dri.leela@gmail.com>
2021-12-17 17:02:10 -0300

    chore(mda-start): add test for createOrUpdateMany

---
+++ packages/mda-start/src/components/User/__tests__/createOrUpdateMany.spec.ts
@@ -0,0 +1,79 @@
+//1. POST to /api/v2/users/create_or_update_many
+//https://developer.zendesk.com/api-reference/ticketing/users/users/#create-or-update-many-users
+
+//2. Com resultado da requisição acima verificar o status GET /api/v2/job_statuses/{job_status_id}
+//https://developer.zendesk.com/api-reference/ticketing/ticket-management/job_statuses/#show-job-status
+
+//3. Resolve requisição quando o status é igual a 'completed'
+
+const mockedFetch = jest.fn() as jest.MockedFunction<typeof fetch>;
+
+jest.mock("cross-fetch", () => ({
+  ...jest.requireActual("cross-fetch"),
+  __esModule: true,
+  fetch: mockedFetch,
+  default: mockedFetch,
+}));
+
+import fetch, { Response } from "cross-fetch";
+import createOrUpdateMany from '../createOrUpdateMany';
+
+describe("Create or Update Many Users", () => {
+
+    jest.setTimeout(30000); 
+  
+    const role: 'end-user' = 'end-user'; 
+    const condition: "inscrita" | "desabilitada" = 'desabilitada';
+    const users = [{
+      name: "Teste",
+      role: role,
+      organization_id: 360269610652,
+      email: "teste@email.com",
+      external_id: "00001",
+      phone: "99999999999",
+      verified: false,
+      user_fields: {
+        cor: null,
+        address: "",
+        cep: '',
+        city: '',
+        latitude: '',
+        longitude: '',
+        state: '',
+        tipo_de_acolhimento: null,
+        condition: condition,
+        whatsapp: "",
+        registration_number: "21312312",
+        occupation_area: null,
+        disponibilidade_de_atendimentos: "1",
+        data_de_inscricao_no_bonde: "2020-05-29T00:28:29.55569"
+      }
+    }]
+  
+    const result = {
+      "job_status": {
+        "id": "a20228d9f081c4dd37fb64871e0f6ead", 
+        "message": "Completed at 2021-12-17 18:28:10 +0000", 
+        "progress": 1, 
+        "results": [{
+          "email": "teste@email.com", 
+          "external_id": "00001", 
+          "id": 389327067551, "status": "Updated"
+        }],
+        "status": "completed", 
+        "total": 1, 
+        "url": "https://mapadoacolhimento.zendesk.com/api/v2/job_statuses/d01228d9f081c4dd37fb64871e0f6eac.json"
+      }
+    }
+    
+    mockedFetch.mockResolvedValueOnce(new Response(JSON.stringify(result), { status: 200 }));
+    mockedFetch.mockResolvedValueOnce(new Response(JSON.stringify(result), { status: 200 }));
+  
+    it("Create a User", async () => {
+      const data = await createOrUpdateMany(users);
+      console.log('Aqui',data);
+      expect(data).toStrictEqual(result);
+    });
+  
+  });
+
