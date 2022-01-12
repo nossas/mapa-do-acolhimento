b281104f68e53693793b3ec0dda06b708cd97a04
Adriele A Secundino <dri.leela@gmail.com>
2022-01-12 12:14:17 -0300

    chore(listener-match)(listener-solidarity): remove throng

--- packages/listener-match/src/main.ts
+++ packages/listener-match/src/main.ts
@@ -1,29 +1,25 @@
 import apmNode from "elastic-apm-node";
-import throng from "throng";
 import { subscriptionSolidarityTickets } from "./graphql/subscriptions";
 import dbg from "./dbg";
 apmNode.start({
-  captureBody: 'all'
+  captureBody: "all"
 });
 
 const log = dbg.extend("main");
 
-throng({
-  workers: 1,
-  start: async (id: number) => {
-    log(`Started worker ${id}`);
-
-    try {
-      log("Fetching solidarity tickets for match...");
-      await subscriptionSolidarityTickets();
-    } catch (err) {
-      log("throng err: ".red, err);
-    }
-
-    process.on("SIGTERM", function() {
-      log(`Worker ${id} exiting`);
-      log("Cleanup here");
-      process.exit();
-    });
+const start = async () => {
+  try {
+    log("Fetching solidarity tickets for match...");
+    await subscriptionSolidarityTickets();
+  } catch (err) {
+    log("throng err: ".red, err);
   }
-});
+
+  process.on("SIGTERM", function() {
+    log(`Exiting`);
+    log("Cleanup here");
+    process.exit();
+  });
+};
+
+start();
--- packages/listener-solidarity/src/main.ts
+++ packages/listener-solidarity/src/main.ts
@@ -1,7 +1,7 @@
 import apmNode from "elastic-apm-node";
-import throng from "throng";
+
 const apm = apmNode.start({
-  captureBody: 'all'
+  captureBody: "all"
 });
 
 import { subscriptionFormEntries } from "./graphql/subscriptions";
@@ -10,36 +10,33 @@ import logger from "./logger";
 
 const log = logger.child({ module: "main" });
 
-throng({
-  workers: 1,
-  start: async (id: number) => {
-    log.info(`Started worker ${id}`);
-    const transaction: any = apm.startTransaction("worker");
+const start = async () => {
+  const transaction: any = apm.startTransaction("worker");
 
-    try {
-      log.info("Fetching solidarity users...");
-      log.info(
-        "Call subscriptions to form_entries... %s",
-        widgets.map(w => w.id)
-      );
-      await subscriptionFormEntries(widgets, apm);
-    } catch (err) {
-      apm.captureError(err + "");
+  try {
+    log.info("Fetching solidarity users...");
+    log.info(
+      "Call subscriptions to form_entries... %s",
+      widgets.map(w => w.id)
+    );
+    await subscriptionFormEntries(widgets, apm);
+  } catch (err) {
+    apm.captureError(err + "");
 
-      transaction.result = 500;
-      transaction.end();
+    transaction.result = 500;
+    transaction.end();
 
-      log.error("throng err: %s", err);
-    }
-
-    process.on("SIGTERM", function() {
-      log.fatal(`Worker ${id} exiting`);
-      log.info("Cleanup here");
-
-      transaction.result = 503;
-      transaction.end();
-
-      process.exit();
-    });
+    log.error("throng err: %s", err);
   }
-});
+
+  process.on("SIGTERM", function() {
+    log.fatal(`Exiting`);
+    log.info("Cleanup here");
+    transaction.result = 503;
+    transaction.end();
+
+    process.exit();
+  });
+};
+
+start();
