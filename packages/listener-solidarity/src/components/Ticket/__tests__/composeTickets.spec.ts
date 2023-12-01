ad9b48d6305b9d91aee8f40f1aca9292ecf94449
Viviane Dias <vivianedias@protonmail.com>
2023-11-30 23:12:21 -0300

    test: adds composeTickets tests

---
+++ packages/listener-solidarity/src/components/Ticket/__tests__/composeTickets.spec.ts
@@ -0,0 +1,17 @@
+describe("composeTickets", () => {
+  it("should create a closed ticket if user condition is 'desabilitada'", () => {
+    expect(true).toStrictEqual(true);
+  });
+
+  it("should create two tickets if 'tipo de acolhimento' is psychological and legal", () => {
+    expect(true).toStrictEqual(true);
+  });
+
+  it("should create a ticket if 'tipo de acolhimento' is psychological", () => {
+    expect(true).toStrictEqual(true);
+  });
+
+  it("should create a ticket if 'tipo de acolhimento' is legal", () => {
+    expect(true).toStrictEqual(true);
+  });
+});
