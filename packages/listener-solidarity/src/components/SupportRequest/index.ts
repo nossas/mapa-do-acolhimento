c9a22f9241bba925420a449e51b4b36540b224e8
Viviane Dias <vivianedias@protonmail.com>
2023-12-05 18:38:00 -0300

    fix(solidarity): sends correct payload to lambda endpoint

--- packages/listener-solidarity/src/components/SupportRequest/index.ts
+++ packages/listener-solidarity/src/components/SupportRequest/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { sign } from "jsonwebtoken";
 import { Ticket, User } from "../../types";
+import logger from "../../logger";
 
 function getSupportType(subject: string) {
   return subject.includes("Psicológico") ? "psychological" : "legal";
@@ -19,37 +20,37 @@ function getSupportRequests(ticket: Ticket, msrs: User[]) {
     hasDisability: false,
     requiresLibras: false,
     acceptsOnlineSupport: true,
-    lat: user.user_fields.latitude,
-    long: user.user_fields.longitude,
+    lat: Number(user.user_fields.latitude),
+    lng: Number(user.user_fields.longitude),
     city: user.user_fields.city,
     state: user.user_fields.state,
   };
 }
 
+const log = logger.child({ labels: { process: "createSupportRequests" } });
+
 export default async function createSupportRequests(
   msrZendeskTickets: (Ticket | undefined)[],
   msrs: User[]
 ) {
+  log.info("Starting createSupportRequests...");
   const validTickets = msrZendeskTickets.filter(Boolean);
   if (validTickets.length < 1 || !validTickets)
     throw new Error("No valid tickets to save as support requests");
 
   const lambdaUrl = process.env["NEW_MATCH_LAMBDA_URL"];
   const jwtSecret = process.env["JWT_SECRET"];
+  const endpoint = `${lambdaUrl}/create`;
   const authToken = sign({ sub: "listener-solidarity" }, jwtSecret!, {
-    expiresIn: 3600, // expires in 1 hour
+    expiresIn: 300, // expires in 5 minutes
   });
   const supportRequests = msrZendeskTickets.map((ticket) =>
     getSupportRequests(ticket as Ticket, msrs)
   );
-  const createSupportRequest = await axios.post(lambdaUrl!, supportRequests, {
+  const createSupportRequest = await axios.post(endpoint, supportRequests, {
     headers: {
       Authorization: authToken,
     },
-  });
-
-  console.log({
-    createSupportRequest: JSON.stringify(createSupportRequest, null, 2),
   });
 
   return createSupportRequest;
--- packages/listener-solidarity/src/components/index.ts
+++ packages/listener-solidarity/src/components/index.ts
@@ -116,10 +116,17 @@ export const handleIntegration = (widgets: Widget[], apm) => async (
     const zendeskTickets = await limiter.schedule(() =>
       createZendeskTickets(tickets)
     );
-    createSupportRequests(zendeskTickets, msrUsers).catch(e => {
-      log.error(`Couldn't create support requests: ${e}`);
-      apm.captureError(e);
-    });
+    createSupportRequests(zendeskTickets, msrUsers)
+      .then(() => log.info("Success creating support requests for tickets!"))
+      .catch((e) => {
+        if (e.response) {
+          log.error(
+            `Couldnt create support requests: ${
+              e.response.status
+            } - ${JSON.stringify(e.response.data)}`
+          );
+        }
+      });
 
     // Save users in Hasura
     insertSolidarityUsers(withoutDuplicates as never).catch(e => {
